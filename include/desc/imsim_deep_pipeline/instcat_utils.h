79dce2b71eef26a4fb26809fa0c8749c4fc07131
Jim Chiang <jchiang@slac.stanford.edu>
2017-01-21 13:11:58 -0800

    add code to manage caching of large instance catalogs

--- include/desc/imsim_deep_pipeline/instcat_utils.h
+++ include/desc/imsim_deep_pipeline/instcat_utils.h
@@ -8,7 +8,8 @@ namespace desc {
 
       double ang_sep(double ra0, double dec0, double ra1, double dec1);
 
-      void sky_cone_select(const std::string & infile,
+      void sky_cone_select(const std::string & obs_par_file,
+                           const std::string & object_file,
                            double ra, double dec, double radius,
                            const std::string & outfile);
    } // namespace imsim_deep_pipeline
