c49499bee79f4024fa59c04fa97bd8c0568e4988
Jim Chiang <jchiang@slac.stanford.edu>
2017-01-21 14:40:45 -0800

    add caching code; update make_instcat to use it

---
+++ python/desc/imsim_deep_pipeline/object_cache_info.py
@@ -0,0 +1,94 @@
+"""
+Module to cache instance catalog object files.
+"""
+from __future__ import absolute_import, print_function
+import pickle
+from collections import namedtuple
+import desc.imsim_deep_pipeline
+
+__all__ = ['ObjectCacheInfo', 'ObjectCacheInfoException']
+
+ObjectCatalog = namedtuple('ObjectCatalog', ('filename', 'ra', 'dec', 'radius'))
+
+
+class ObjectCacheInfoException(RuntimeError):
+    pass
+
+
+class ObjectCacheInfo(object):
+    """
+    Class to cache large instance catalog object files.
+    """
+    def __init__(self):
+        self.catalogs = []
+
+    def add_obj_file(self, object_file, radius):
+        self.catalogs.append(self.make_object_catalog(object_file, radius))
+
+    @staticmethod
+    def make_object_catalog(object_file, radius):
+        ra, dec = ObjectCacheInfo._extract_ra_dec(object_file)
+        return ObjectCatalog(object_file, ra, dec, radius)
+
+    @staticmethod
+    def _extract_ra_dec(object_file):
+        ra, dec = None, None
+        with open(object_file) as input_:
+            for line in input_:
+                if line.startswith('rightascension'):
+                    ra = float(line.strip().split()[1])
+                if line.startswith('declination'):
+                    dec = float(line.strip().split()[1])
+                if ra is not None and dec is not None:
+                    break
+        if ra is None or dec is None:
+            raise RuntimeError("RA or Dec not found in %s" % object_file)
+        return ra, dec
+
+    def get_object_files(self, candidate_file, radius):
+        """Find the instance catalog object files that cover the candidate
+        file.
+
+        Parameters
+        ----------
+        candidate_file : str
+            Filename of observing parameters-only instance catalog that
+            needs to be covered by the cached catalogs.
+        radius : float
+            Radius (degrees) of the candidate instance catalog.
+
+        Returns
+        -------
+        list
+            A list of the covering catalog filenames.
+
+        Raises
+        ------
+        ObjectCacheInfoException
+            If the candidate catalog cannot be covered by the cached
+            catalogs.
+
+        Notes
+        -----
+        Currently, this function only returns lists containing a
+        single catalog, i.e., coverage for candidate catalogs by
+        multiple catalog is not yet supported.
+        """
+        candidate = self.make_object_catalog(candidate_file, radius)
+        for catalog in self.catalogs:
+            sep = desc.imsim_deep_pipeline.ang_sep(candidate.ra, candidate.dec,
+                                                   catalog.ra, catalog.dec)
+            if sep <= catalog.radius - candidate.radius:
+                return [catalog.filename]
+        message = ("Candidate catalog %s cannot be covered by cached catalogs."
+                   % candidate_file)
+        raise ObjectCacheInfoException(message)
+
+    def save(self, outfile):
+        with open(outfile, 'wb') as output:
+            pickle.dump(self, output)
+
+    @staticmethod
+    def load(infile):
+        with open(infile, 'rb') as input_:
+            return pickle.load(input_)
---
+++ tests/test_object_cache_info.py
@@ -0,0 +1,111 @@
+"""
+Unit tests for instance catalog caching code.
+"""
+import os
+import unittest
+import desc.imsim_deep_pipeline
+
+class ObjectCacheInfoTest(unittest.TestCase):
+    "Test case for ObjectCacheInfo class."
+    def setUp(self):
+        self.object_info_cache = desc.imsim_deep_pipeline.ObjectCacheInfo()
+
+        ra, dec = 93.1078762, -29.9663229
+        self.large_file = 'large_object_file.txt'
+        self.large_file_radius = 0.1
+        self._make_instcat_file(self.large_file, ra, dec)
+        self.object_info_cache.add_obj_file(self.large_file,
+                                            self.large_file_radius)
+
+        self.offset = self.large_file_radius/2.
+        self.offset_file = 'offset_object_file.txt'
+        self._make_instcat_file(self.offset_file, ra, dec + self.offset)
+
+        self.disjoint_file = 'disjoint_object_file.txt'
+        self._make_instcat_file(self.disjoint_file, ra,
+                                dec + 2*self.large_file_radius)
+
+    def tearDown(self):
+        del self.object_info_cache
+        for cat_file in (self.large_file, self.offset_file, self.disjoint_file):
+            try:
+                os.remove(cat_file)
+            except OSError:
+                pass
+
+    @staticmethod
+    def _make_instcat_file(filename, ra, dec):
+        with open(filename, 'w') as output:
+            output.write('rightascension %.7f\n' % ra)
+            output.write('declination %.7f\n' % dec)
+
+    def test_caching(self):
+        """
+        Test for caching based on file pointing directions and trim
+        radius of object list.
+        """
+        # Test for matching file
+        radius = self.large_file_radius
+        object_files \
+            = self.object_info_cache.get_object_files(self.large_file, radius)
+        self.assertEqual(object_files[0], self.large_file)
+
+        # Test for centered contained file.
+        radius = self.large_file_radius/2.
+        object_files \
+            = self.object_info_cache.get_object_files(self.large_file, radius)
+        self.assertEqual(object_files[0], self.large_file)
+
+        # Test for offset contained file.
+        radius = self.offset/10.
+        object_files \
+            = self.object_info_cache.get_object_files(self.offset_file, radius)
+        self.assertEqual(object_files[0], self.large_file)
+
+        # Test for file which is larger than cached file.
+        radius = self.large_file_radius*2.
+        self.assertRaises(desc.imsim_deep_pipeline.ObjectCacheInfoException,
+                          self.object_info_cache.get_object_files,
+                          self.large_file, radius)
+
+        # Test for uncontained offset file.
+        radius = self.large_file_radius
+        self.assertRaises(desc.imsim_deep_pipeline.ObjectCacheInfoException,
+                          self.object_info_cache.get_object_files,
+                          self.offset_file, radius)
+
+        # Test for disjoint file.
+        radius = self.large_file_radius
+        self.assertRaises(desc.imsim_deep_pipeline.ObjectCacheInfoException,
+                          self.object_info_cache.get_object_files,
+                          self.disjoint_file, radius)
+
+    def test_add_object_file(self):
+        "Test the addition of another object file to the cache."
+        radius = 2.1
+        self.object_info_cache.add_obj_file(self.disjoint_file, radius)
+        object_files \
+            = self.object_info_cache.get_object_files(self.disjoint_file,
+                                                      radius/2.)
+
+        self.assertEqual(len(object_files), 1)
+        self.assertEqual(object_files[0], self.disjoint_file)
+
+    def test_persistence(self):
+        "Test methods to save and load the caching data to and from disk."
+        radius = 2.1
+        self.object_info_cache.add_obj_file(self.disjoint_file, radius)
+        cache_file = 'my_object_catalog_cache'
+        self.object_info_cache.save(cache_file)
+        my_object_cache \
+            = desc.imsim_deep_pipeline.ObjectCacheInfo.load(cache_file)
+
+        object_files \
+            = self.object_info_cache.get_object_files(self.disjoint_file,
+                                                      radius/2.)
+        self.assertEqual(len(object_files), 1)
+        self.assertEqual(object_files[0], self.disjoint_file)
+        os.remove(cache_file)
+
+if __name__ == '__main__':
+    unittest.main()
