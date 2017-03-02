cb2e4137556bd9f81ef333ccac7eb506443745ff
Jim Chiang <jchiang@slac.stanford.edu>
2017-03-02 09:23:23 -0800

    update slac setup script with location of Stack distribution with galsim bug fix

--- setup/slac_setup.sh
+++ setup/slac_setup.sh
@@ -1,4 +1,4 @@
-source /gpfs/slac/kipac/fs1/g/desc/imsim_deep/DMstack/LSST_Stack_2017-01-19/loadLSST.bash
+source /nfs/farm/g/desc/u1/software/redhat6-x86_64-64bit-devtoolset-3/DMstack/twinkles_20170221/loadLSST.bash
 export SLAC_INSTALL_DIR=/nfs/farm/g/lsst/u1/software/redhat6-x86_64-64bit-gcc44
 export PATH=${SLAC_INSTALL_DIR}/git/1.8.4.5/bin:${SLAC_INSTALL_DIR}/git/git-lfs-1.4.1:$PATH
 echo 'added /nfs/farm/g/lsst/u1/software/redhat6-x86_64-64bit-gcc44/anaconda/py-2.7.11/bin, git 1.8.4.5, and git-lfs 1.4.1 to PATH'
@@ -6,4 +6,4 @@ echo 'added /nfs/farm/g/lsst/u1/software/redhat6-x86_64-64bit-gcc44/anaconda/py-
 setup lsst_sims
 setup -r /gpfs/slac/kipac/fs1/g/desc/imsim_deep/imSim -j
 setup -r /gpfs/slac/kipac/fs1/g/desc/imsim_deep/imsim_deep_pipeline -j
-PS1="[gpfs_stack] "
+PS1="[twinkles_2017-02-21_stack] "
