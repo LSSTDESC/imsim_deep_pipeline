6babe3d3f4716db7f5824f65acf1e5b2b98bbdfb
Jim Chiang <jchiang@slac.stanford.edu>
2017-04-01 15:59:55 -0700

    use master of cloned sims_GalSimInterface for psf fixes

--- setup/slac_setup.sh
+++ setup/slac_setup.sh
@@ -6,6 +6,7 @@ export PATH=${SLAC_INSTALL_DIR}/git/1.8.4.5/bin:${SLAC_INSTALL_DIR}/git/git-lfs-
 echo 'added /nfs/farm/g/lsst/u1/software/redhat6-x86_64-64bit-gcc44/anaconda/py-2.7.11/bin, git 1.8.4.5, and git-lfs 1.4.1 to PATH'
 
 setup lsst_sims
+setup -r /gpfs/slac/kipac/fs1/g/desc/imsim_deep/sims_psf_fixes/sims_GalSimInterface -j
 setup -r /gpfs/slac/kipac/fs1/g/desc/imsim_deep/imSim -j
 setup -r /gpfs/slac/kipac/fs1/g/desc/imsim_deep/imsim_deep_pipeline -j
 
